81e19572f2300e65479f8207f2a58d80b8635757
Furqan Ali <furqanali2628@gmail.com>
2026-09-23 01:37:42 +0500

    Cycle 10: expose new public API

--- counting_game_core/__init__.py
+++ counting_game_core/__init__.py
@@ -4,3 +4,5 @@ from .analytics import SessionStats, accuracy, longest_correct_streak
 from .session_metrics import session_metrics
 
 __all__ = ["SessionStats", "accuracy", "longest_correct_streak", "session_metrics"]
+
+from .config import GameConfig
